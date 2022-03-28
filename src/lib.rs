4ae559673578d228445935cf7c647b5b987c9217
Matthew Pagan <mastapegs01@gmail.com>
2022-03-27 20:16:57 -0400

    feat: completed odd_or_even

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ mod even_fib;
 mod multiple_3_5;
 mod power_of_two;
 mod string_ends_with;
+mod odd_or_even;
---
+++ src/odd_or_even.rs
@@ -0,0 +1,41 @@
+#[allow(dead_code)]
+fn odd_or_even(numbers: Vec<i32>) -> String {
+    match numbers.into_iter().sum::<i32>() % 2 {
+        0 => "even".to_string(),
+        _ => "odd".to_string(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_empty_array() {
+        assert_eq!(odd_or_even(vec![]), "even");
+    }
+    #[test]
+    fn test_single_even() {
+        assert_eq!(odd_or_even(vec![0]), "even");
+    }
+    #[test]
+    fn test_single_odd() {
+        assert_eq!(odd_or_even(vec![1]), "odd");
+    }
+    #[test]
+    fn test_even() {
+        assert_eq!(odd_or_even(vec![0, 1, 5]), "even");
+    }
+    #[test]
+    fn test_odd() {
+        assert_eq!(odd_or_even(vec![0, 1, 4]), "odd");
+    }
+    #[test]
+    fn test_negative_even() {
+        assert_eq!(odd_or_even(vec![0, -1, -5]), "even");
+    }
+    #[test]
+    fn test_negative_odd() {
+        assert_eq!(odd_or_even(vec![0, -1, 2]), "odd");
+    }
+}
