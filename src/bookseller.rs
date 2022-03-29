7c575d78774fd41b7b996926522ba471320f0dcd
Matthew Pagan <mastapegs01@gmail.com>
2022-03-29 12:53:46 -0400

    feat: bookseller

---
+++ src/bookseller.rs
@@ -0,0 +1,50 @@
+#[allow(dead_code)]
+fn stock_list(list_art: Vec<&str>, list_cat: Vec<&str>) -> String {
+    list_cat
+        .iter()
+        .map(|category| {
+            format!(
+                "({category} : {count})",
+                count = {
+                    list_art
+                        .iter()
+                        .filter(|book| book.starts_with(category))
+                        .map(|book| {
+                            book.split_whitespace().collect::<Vec<&str>>()[1]
+                                .parse::<i32>()
+                                .unwrap()
+                        })
+                        .sum::<i32>()
+                }
+            )
+        })
+        .collect::<Vec<String>>()
+        .join(" - ")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn dotest(list_art: Vec<&str>, list_cat: Vec<&str>, exp: &str) -> () {
+        println!("list_art: {:?};", list_art);
+        println!("list_cat: {:?};", list_cat);
+        let ans = stock_list(list_art, list_cat);
+        println!("actual:\n{:?};", ans);
+        println!("expect:\n{:?};", exp);
+        println!("{};", ans == exp);
+        assert_eq!(ans, exp);
+        println!("{};", "-");
+    }
+
+    #[test]
+    fn basic_tests() {
+        let mut b = vec!["BBAR 150", "CDXE 515", "BKWR 250", "BTSQ 890", "DRTY 600"];
+        let mut c = vec!["A", "B", "C", "D"];
+        dotest(b, c, "(A : 0) - (B : 1290) - (C : 515) - (D : 600)");
+
+        b = vec!["ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"];
+        c = vec!["A", "B"];
+        dotest(b, c, "(A : 200) - (B : 1140)");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 mod automorphic_number;
+mod bookseller;
 mod create_phone_number;
 mod even_fib;
 mod maximum_product;
